7f514438bcb4f6662df364e3102abf7125b55870
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-09 10:03:24 +0400

    Added task29

---
+++ task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] createArray(int n){
+    int[] array = new int[n];
+    for(int i = 0; i < n; i++){
+        array[i] = new Random().Next(-100, 100);
+    }
+
+    return array;
+}
+
+void printArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = createArray(8);
+printArray(arr);
